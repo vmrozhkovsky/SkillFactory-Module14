6ef1570fd3d8eb7219eb73500e298ff3ad9fad89
vmrozhkovsky <128725885+vmrozhkovsky@users.noreply.github.com>
2023-06-24 17:27:28 +0300

    Добавлен проект PhoneBook

---
+++ LINQ/PhoneBook/Contact.cs
@@ -0,0 +1,17 @@
+﻿namespace PhoneBook;
+
+public class Contact
+{
+    public Contact(string name, string lastName, long phoneNumber, String email)
+    {
+        Name = name;
+        LastName = lastName;
+        PhoneNumber = phoneNumber;
+        Email = email;
+    }
+  
+    public String Name {get;}
+    public String LastName {get;}
+    public long PhoneNumber {get;}
+    public String Email {get;}
+}
--- LINQ/PhoneBook/Program.cs
+++ LINQ/PhoneBook/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using PhoneBook;
 
-Console.WriteLine("Hello, World!");
+var phoneBook = new List<Contact>();
+
+phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+phoneBook.Add(new Contact("Сергей", "Довлатов",79990000010, "serge@example.com"));
+phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+phoneBook.Add(new Contact("Валерий", "Леонтьев",79990000012, "valera@example.com"));
+phoneBook.Add(new Contact("Сергей", "Брин",  799900000013, "serg@example.com"));
+phoneBook.Add(new Contact("Иннокентий", "Смоктуновский",799900000013, "innokentii@example.com"));
+
+var sortedPhoneBook = phoneBook.OrderBy(b => b.Name).ThenBy(b => b.LastName);
+foreach(var b in sortedPhoneBook)
+    Console.WriteLine($"{b.Name} {b.LastName}");
